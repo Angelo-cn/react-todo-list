7b75f503d9b670424418c5f6649421ec43a01309
Angelo-cn <eaconde05@gmail.com>
2024-07-15 23:56:23 -0300

    Tailwind configuration succed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 function App() {
   return (
-    <>
-      <h1>Lorem ipsum dolor sit amet.</h1>
-    </>
-  )
+    <div className="h-screen bg-slate-900">
+      <div className="max-w-4xl mx-auto pt-20 px-6">
+        <h1 className="text-3xl text-slate-100 mb-4">La To-do liste</h1>
+
+        <form className="mb-10">
+          <label htmlFor="todo-item" className="text-slate-50">
+            Ajouter une chose à faire
+          </label>
+          <input type="text" className="mt-1 block w-full rounded" />
+          <button className="mt-4 py-2 bg-slate-50 rounded min-w-[115px]">
+            Ajouter
+          </button>
+        </form>
+        <ul>
+          
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
